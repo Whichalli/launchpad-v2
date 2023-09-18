9282ff403114e4050c62e7611624471aed698236
Whichalli <137446284+Whichalli@users.noreply.github.com>
2023-09-18 08:43:54 +0400

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [
+        '@safe-globalThis/safe-ethers-adapters', // Add this line
+        '@thirdweb-dev/wallets',
+        '@safe-global/safe-core-sdk',
+      ],
+    },
+  },
+  
   plugins: [react()],
   define: {
     global: "globalThis",
